1dbca208b73a157babe9824f8b13725832b6a96d
matuirbayasik317 <sdt2395gp0002@edu.sca.ac.jp>
2023-11-30 12:31:59 +0900

    Iwasaのシーンの追加と、scroll（横移動）のスクリプトの実装シリアライズで作成したため設定しなおす

---
+++ Assets/Iwasa/script/Scroll.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    void Update()
+    {
+        //背景もしくはグラウンドにコンポーネントしといて、Serializeで設定したからスピードの設定も忘れずに
+        transform.position -= new Vector3(Time.deltaTime * speed,0);
+        if(transform.position.x <= -8.9f)
+        {
+            transform.position = new Vector3(8.9f,0);
+        }
+    }
+}
