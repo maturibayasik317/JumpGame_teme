00a740e92c51805e5a4be19d64f118c8e15c777a
maturibayasik317 <sdt2395gp0002@edu.sca.ac.jp>
2023-12-07 10:45:46 +0900

    ジャンプできるようにした

--- Assets/Iwasa/script/Jump_Dash.cs
+++ Assets/Iwasa/script/Jump_Dash.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Jump_Dash : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 velocity;
+    [SerializeField] float Jump_Power;//ジャンプ力
+    private int Jump_Count =1;
+    private int JumpCount = 0;
+    private bool jump = false;
+    Rigidbody2D rigid2D;
+    [SerializeField] ContactFilter2D filter2D;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if ( JumpCount < Jump_Count && Input.GetKey(KeyCode.Space))
+        {
+            jump = true;
+        }
+        if (jump)
+        { 
+            this.rigid2D = GetComponent<Rigidbody2D>();
+            rigid2D.velocity = Vector2.zero;
+
+            //ジャンプさせる
+            this.rigid2D.AddForce(transform.up * Jump_Power);
+            JumpCount++;
+
+            jump = false;
+        }
+        if (GetComponent<Rigidbody2D>().IsTouching(filter2D))
+        {
+            //地面についたら終わったらもう一回できるようにする
+            JumpCount = 0;
+        }
     }
 }
