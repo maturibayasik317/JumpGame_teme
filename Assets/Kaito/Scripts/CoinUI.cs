21838058555aeb434c1cf31f315fb4f9dc18faef
kaito310 <7415kaito.s@gmail.com>
2024-01-18 09:47:14 +0900

    コインのUI作成完了

--- Assets/Kaito/Scripts/CoinUI.cs
+++ Assets/Kaito/Scripts/CoinUI.cs
@@ -6,9 +6,11 @@ using UnityEngine.UI;
 //----獲得したコインの数を表示するUI----
 public class CoinUI : MonoBehaviour
 {
-    [SerializeField] GameObject[] stageCoin; // ステージ上に配置するコイン
-    [SerializeField] GameObject[] uiCoin; // UIとして表示するコイン
-    [SerializeField] Sprite coinSprite; // コイン取得後の画像
+    [SerializeField] GameObject[] stageCoins; // ステージ上に配置するコイン
+    
+    [SerializeField] Sprite coinSprite; // コイン画像
+    [SerializeField] Image[] coinImages; // UIとして表示するコイン
+    
     // 獲得したコイン数を表示するテキスト
     [SerializeField] Text coinNumText;
 
@@ -17,17 +19,29 @@ public class CoinUI : MonoBehaviour
     void Start()
     {
         playerCoinScript = GameObject.Find("Player").GetComponent<PlayerGetCoin>();
-        
     }
 
     void Update()
     {
         // 獲得したコイン数 / ステージに配置したコイン数
-        coinNumText.text = $"Coin：{playerCoinScript.GetPlayerCoin}／{stageCoin.Length}";
+        coinNumText.text = $"Coin：{playerCoinScript.GetPlayerCoin}／{stageCoins.Length}";
+        ChangeSprite();
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    // コイン画像に変更する
+    void ChangeSprite()
+    {
+        if (!stageCoins[0])
         {
-            
+            coinImages[0].sprite = coinSprite;
+        }
+        if (!stageCoins[1])
+        {
+            coinImages[1].sprite = coinSprite;
+        }
+        if (!stageCoins[2])
+        {
+            coinImages[2].sprite = coinSprite;
         }
     }
 }
